b965ed72654f2144da2426e0a2a498506d12edda
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-11-13 23:12:33 +0200

    upload the basis

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Login from './loginAndSignup/login.js';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/loginAndSignup/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css';
+
+
+function login() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default login;
