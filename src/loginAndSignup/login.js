5a631fa335201190829be76409696744ff8a0e0f
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-11-15 00:34:18 +0200

    nothing important

--- src/loginAndSignup/login.js
+++ src/loginAndSignup/login.js
@@ -5,15 +5,11 @@ import {FaGithub, FaFacebook, FaLinkedin} from "react-icons/fa";
 import classNames from "classnames";
 
 
-
-
 function Login() {
-         const [isLoginActive, setIsLoginActive] = useState(false);
-
-
+    const [isLoginActive, setIsLoginActive] = useState(false);
     return (
         <div className="Body">
-            <div className= {classNames({ "Container":true,"active":isLoginActive })} id="container">
+            <div className={classNames({"Container": true, "active": isLoginActive})} id="container">
                 <div className="form-container sign-up-container">
                     <form action="#">
                         <h2>Create Account</h2>
@@ -22,7 +18,7 @@ function Login() {
                             <a href="#" className="social"><FaGithub className="social-icon"/></a>
                             <a href="#" className="social"><FaLinkedin className="social-icon"/></a>
                         </div>
-                        <span>or use your email for registration or Gmail</span>
+                        <span>or use your Gmail to register</span>
                         <input type="text" placeholder="Name"/>
                         <input type="email" placeholder="Email"/>
                         <input type="password" placeholder="Password"/>
@@ -50,12 +46,14 @@ function Login() {
                         <div className="overlay-panel overlay-left">
                             <h2>Welcome Back!</h2>
                             <p>To keep connected with us please login with your personal info</p>
-                            <Button variant="contained" className="ghost" id="signIn" onClick={() => setIsLoginActive(false)} >Sign In</Button>
+                            <Button variant="contained" className="ghost" id="signIn"
+                                    onClick={() => setIsLoginActive(false)}>Sign In</Button>
                         </div>
                         <div className="overlay-panel overlay-right">
                             <h2>Hello, My Friend!</h2>
                             <p>Enter your details and start journey with us</p>
-                            <Button variant="contained" className="ghost" id="signUp" onClick={ () => setIsLoginActive(true)}>Sign Up</Button>
+                            <Button variant="contained" className="ghost" id="signUp"
+                                    onClick={() => setIsLoginActive(true)}>Sign Up</Button>
                         </div>
                     </div>
                 </div>
