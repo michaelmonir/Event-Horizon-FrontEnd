0f879d7071f175fed206c459908d831f8a2ca0cb
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-11-26 22:22:17 +0200

    add profile omponent

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 // eslint-disable-next-line no-unused-vars
 import Login from './loginAndSignup/login.js';
+import Profile from "./porfile/profile";
 
 
 function App() {
   return (
     <div className="App">
-        <Login />
+        <Profile/>
     </div>
   );
 }
