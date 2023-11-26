d55bd29e35d24cb02e471688ae3f12d81f970b60
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-11-26 17:45:09 +0200

    changes

--- src/dashboard/event-modal.js
+++ src/dashboard/event-modal.js
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import TextField from "@mui/material/TextField";
-import Location from "./location";
 import Autocomplete from "@mui/material/Autocomplete";
+import {City, Country, State} from "country-state-city";
 
 let eventCategoriesMap = new Map([
     ["Social Events", ["Weddings", "Parties", "Reunions", "Celebrations"]],
@@ -39,6 +39,18 @@ export default function BasicModal() {
     const handleClose = () => setOpen(false);
     const [eventCategory, setEventCategory] = React.useState("");
     const [eventSubCategory, setEventSubCategory] = React.useState("");
+    let countries = Country.getAllCountries();
+    let states = State.getAllStates();
+    let cities = City.getAllCities();
+    let countryNames = countries.map((country) => country.name);
+    let countryAndID = countries.map((country) => ({name: country.name, isoCode: country.isoCode}));
+
+    const [country, setCountry] = React.useState("");
+    const [state, setState] = React.useState("");
+    const [city, setCity] = React.useState("");
+    const [statesInCountry, setStatesInCountry] = React.useState([]);
+    const [citiesInState, setCitiesInState] = React.useState([] );
+
 
     return (
         <div>
@@ -64,7 +76,68 @@ export default function BasicModal() {
                         <form className="modal-form" action="#">
                             <TextField id="outlined-basic" label="Event Name" variant="outlined" required={true}
                                        helperText="please enter the Event Name"/>
-                            <Location/>
+                            <div className="flex location">
+                                <Autocomplete
+                                    disablePortal
+                                    value={country}
+                                    id="combo-box-demo"
+                                    options= {countryNames}
+                                    sx={{width: 289}}
+                                    renderInput={(params) => <TextField {...params} label="Country"/>}
+                                    onChange={(event, value) => {
+                                        if(!value){
+                                            setCountry(null);
+                                            setState(null);
+                                            setCity(null);
+                                            setStatesInCountry([]);
+                                            setCitiesInState([]);
+                                            return;
+                                        }
+                                        setCountry(value);
+                                        let countryID = countryAndID.find((country) => country.name === value).isoCode;
+                                        let statesInCountry = State.getStatesOfCountry(countryID);
+                                        let stateNames = statesInCountry.map((state) => state.name);
+                                        setStatesInCountry(stateNames);
+                                    }}
+                                />
+                                <Autocomplete
+                                    disablePortal
+                                    value={state}
+                                    id="combo-box-demo"
+                                    options={statesInCountry}
+                                    sx={{width: 300}}
+                                    renderInput={(params) => <TextField {...params} label="State"/>}
+                                    onChange={(event, value) => {
+                                        if(!value){
+                                            setState(null);
+                                            setCity(null);
+                                            setCitiesInState([]);
+                                            return;
+                                        }
+                                        setState(value);
+                                        let stateID = states.find((state) => state.name === value).id;
+                                        let citiesInState = City.getCitiesOfState(stateID);
+                                        let cityNames = citiesInState.map((city) => city.name);
+                                        setCitiesInState(cityNames);
+                                    }}
+
+                                />
+                                <Autocomplete
+                                    disablePortal
+                                    id="combo-box-demo"
+                                    value={city}
+                                    options={citiesInState}
+                                    sx={{width: 300}}
+                                    renderInput={(params) => <TextField {...params} label="City"/>}
+                                    onChange={(event, value) => {
+                                        if(!value){
+                                            setCity(null);
+                                            return;
+                                        }
+                                        setCity(value);
+                                    }}
+                                />
+                            </div>
                             <Autocomplete
                                 disablePortal
                                 id="combo-box-demo"
@@ -106,7 +179,7 @@ export default function BasicModal() {
                             />
                             <TextField
                                 id="datetime-local"
-                                label="Next appointment"
+                                label="Event Date"
                                 defaultValue={new Date().toISOString().slice(0, 16)}
                                 type="datetime-local"
                                 InputLabelProps={{
