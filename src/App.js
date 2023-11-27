bab9c9d2d18cdbacc5bb1d6f2553982c1051da33
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-11-27 16:09:07 +0200

    add a validation for the input pattern for sign up and login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-// eslint-disable-next-line no-unused-vars
 import Login from './loginAndSignup/login.js';
+// eslint-disable-next-line no-unused-vars
 import ValidationPage from "./validation/validationPage";
 function App() {
   return (
     <div >
-        <ValidationPage />
+        <Login/>
     </div>
 
   );
