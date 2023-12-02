ac45099704706c8d675dbfcd63e5c62530b4ddd8
Michael <michaael.monir119@gmail.com>
2023-12-02 14:58:48 +0200

    divided the attributes in the profile into separate components

--- src/porfile/profile.js
+++ src/porfile/profile.js
@@ -1,15 +1,10 @@
 import React, {useEffect} from 'react';
 import './profie.css';
-import {IoHome} from "react-icons/io5";
-
-import {GiMailbox} from "react-icons/gi";
-import {FaBookmark} from "react-icons/fa";
-import {RiLogoutBoxLine} from "react-icons/ri";
 import BasicModal from "./profile-update-modal";
 import informationApis from "../Apis/UserApis/InformationApis";
-import {getUserId, removeUserLocalStorageData} from "../Authentication/UserAuthentication";
-import {RoutePathNames} from "../Routes/RoutePathNames";
+import {getUserId} from "../Authentication/UserAuthentication";
 import {ProfileSideMenu} from "./ProfileSideMenu";
+import {ProfileAttributeComponent} from "./ProfileAttributeComponent";
 
 
 function Profile() {
@@ -57,64 +52,15 @@ function Profile() {
                     </div>
                 </div>
                 <div className="profile-main-content-body">
-                    <div className="profile-info-container"
-                         style={{
-                             borderTopLeftRadius: "35px",
-                             borderTopRightRadius: "35px",
-                         }}
-                    >
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">UserName:</div>
-                            <div className="profile-info-item-value">{profileAttributes.userName}</div>
-                        </div>
 
-                    </div>
-                    <div className="profile-info-container">
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">first name:</div>
-                            <div className="profile-info-item-value">{profileAttributes.firstName}</div>
-                        </div>
-
-                    </div>
-                    <div className="profile-info-container">
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">last name:</div>
-                            <div className="profile-info-item-value">{profileAttributes.lastName}</div>
-                        </div>
-
-                    </div>
-                    <div className="profile-info-container">
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">Role:</div>
-                            <div className="profile-info-item-value">{profileAttributes.role}</div>
-                        </div>
-
-                    </div>
-                    <div className="profile-info-container">
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">Email:</div>
-                            <div className="profile-info-item-value">
-                                {profileAttributes.email}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="profile-info-container">
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">gender:</div>
-                            <div className="profile-info-item-value">{profileAttributes.gender}</div>
-                        </div>
-                    </div>
-                    <div className="profile-info-container"
-                         style={{
-                             borderBottomLeftRadius: "35px",
-                             borderBottomRightRadius: "35px",
-                         }}
-                    >
-                        <div className="profile-info-item">
-                            <div className="profile-info-item-title">paypal account:</div>
-                            <div className="profile-info-item-value"> {profileAttributes.paypalAccount}</div>
-                        </div>
-                    </div>
+                    <ProfileAttributeComponent attributeName={"UserName"} attributeValue={profileAttributes.userName} />
+                    <ProfileAttributeComponent attributeName={"First Name"} attributeValue={profileAttributes.firstName} />
+                    <ProfileAttributeComponent attributeName={"Last Name"} attributeValue={profileAttributes.lastName} />
+                    <ProfileAttributeComponent attributeName={"Last Name"} attributeValue={profileAttributes.lastName} />
+                    <ProfileAttributeComponent attributeName={"Role"} attributeValue={profileAttributes.role} />
+                    <ProfileAttributeComponent attributeName={"Email"} attributeValue={profileAttributes.email} />
+                    <ProfileAttributeComponent attributeName={"Gender"} attributeValue={profileAttributes.gender} />
+                    <ProfileAttributeComponent attributeName={"Paypal Account"} attributeValue={profileAttributes.gender} />
                 </div>
             </div>
         </div>
